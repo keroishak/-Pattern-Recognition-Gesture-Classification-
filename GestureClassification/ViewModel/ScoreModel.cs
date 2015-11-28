030a63f1c6baaba20f2d112605df75a5f52d9522
keroishak <you@example.com>
2015-11-28 15:19:46 +0200

    Infrastructure and Dataset

---
+++ GestureClassification/ViewModel/ScoreModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace GestureClassification.ViewModel
+{
+    static class ScoreModel
+    {
+       
+    }
+}
---
+++ GestureClassification/ViewModel/TrainModel.cs
@@ -0,0 +1,16 @@
+﻿//This Class 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+namespace GestureClassification.ViewModel
+{
+    class TrainModel
+    {
+       
+    }
+}
